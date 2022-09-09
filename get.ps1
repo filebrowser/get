8f92f2e0a85fb5b0c56f12d73ba7854e9db2a459
John Guan <Guan.Ningchuan@gmail.com>
2022-09-09 18:59:59 +0800

    Update get.ps1 (#19)
    Update Usage Link

--- get.ps1
+++ get.ps1
@@ -7,7 +7,7 @@
 #
 #   Usage:
 #
-#   	iwr -useb https://filebrowser.xyz/get.ps1 | iex
+#   	iwr -useb https://raw.githubusercontent.com/filebrowser/get/master/get.ps1 | iex
 #
 #	You should run it as administrator so it can add filemanager to 
 #	the PATH.
